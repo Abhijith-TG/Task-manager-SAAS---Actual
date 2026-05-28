8496c6f2683b087759c6ed55e9796671c7a24e07
Abhijith <abhijithtg62@gmail.com>
2026-05-28 19:08:31 +0530

    feat: Added controller for create and login user

---
+++ saas-task-manager-be/src/api/controllers/user.controller.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from "express"
+
+
+export const createUserController=(req:Request,res:Response)=>{
+    try{
+
+    }catch(error:any){
+
+    }
+}
+
+export const loginUserController=(req:Request,res:Response)=>{
+    try{
+
+    }catch(error:any){
+        
+    }
+}
